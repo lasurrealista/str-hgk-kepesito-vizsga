8dcd1eafe1fe21497b10daf7d1236bf8ecaede0f
Evelin <ladyblue@freemail.hu>
2021-09-06 12:14:23 +0200

    2nd task

--- backend/src/models/school.model.js
+++ backend/src/models/school.model.js
@@ -1,3 +1,49 @@
 /**
  * Exercise: make School mongoose model
  */
+
+ const mongoose = require('mongoose');
+
+ const SchoolSchema = mongoose.Schema({
+     _id: {
+         type: String,
+         required: false
+     },
+     name: {
+         type: String,
+         required: true
+     },
+     city: {
+        type: String,
+        required: true
+    },
+    street: {
+        type: String,
+        required: true
+    },
+    zipcode: {
+        type: Number,
+        required: true
+    },
+    classrooms: {
+        type: [String],
+        required: true
+    },
+ }, {
+     timeStamps: true
+ });
+ 
+ module.exports = mongoose.model('school', SchoolSchema);
+ 
+
+
+/*
+export interface School {
+  _id?:string;
+  name:string;
+  city: string;
+  street: string;
+  zipcode: number;
+  classrooms?: string[];  // references for classroom entities
+}
+*/
